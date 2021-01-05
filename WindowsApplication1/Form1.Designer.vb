1ee523f8514b4824d6cefc12562cda1c4439564f
ayushegupta <ayushigupta1414@gmail.com>
2021-01-05 16:33:17 +0530

    initial commit

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.no1 = New System.Windows.Forms.Button()
+        Me.no2 = New System.Windows.Forms.Button()
+        Me.no3 = New System.Windows.Forms.Button()
+        Me.no4 = New System.Windows.Forms.Button()
+        Me.no5 = New System.Windows.Forms.Button()
+        Me.no6 = New System.Windows.Forms.Button()
+        Me.no7 = New System.Windows.Forms.Button()
+        Me.no8 = New System.Windows.Forms.Button()
+        Me.no9 = New System.Windows.Forms.Button()
+        Me.no0 = New System.Windows.Forms.Button()
+        Me.Button_decimal = New System.Windows.Forms.Button()
+        Me.Button_add = New System.Windows.Forms.Button()
+        Me.Button_ans = New System.Windows.Forms.Button()
+        Me.Button_sub = New System.Windows.Forms.Button()
+        Me.Button_multi = New System.Windows.Forms.Button()
+        Me.Button_divide = New System.Windows.Forms.Button()
+        Me.Input = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'no1
+        '
+        Me.no1.AutoSize = True
+        Me.no1.Location = New System.Drawing.Point(132, 182)
+        Me.no1.Name = "no1"
+        Me.no1.Size = New System.Drawing.Size(57, 50)
+        Me.no1.TabIndex = 0
+        Me.no1.Text = "1"
+        Me.no1.UseVisualStyleBackColor = True
+        '
+        'no2
+        '
+        Me.no2.AutoSize = True
+        Me.no2.Location = New System.Drawing.Point(72, 182)
+        Me.no2.Name = "no2"
+        Me.no2.Size = New System.Drawing.Size(54, 50)
+        Me.no2.TabIndex = 1
+        Me.no2.Text = "2"
+        Me.no2.UseVisualStyleBackColor = True
+        '
+        'no3
+        '
+        Me.no3.AutoSize = True
+        Me.no3.Location = New System.Drawing.Point(6, 182)
+        Me.no3.Name = "no3"
+        Me.no3.Size = New System.Drawing.Size(60, 49)
+        Me.no3.TabIndex = 2
+        Me.no3.Text = "3"
+        Me.no3.UseVisualStyleBackColor = True
+        '
+        'no4
+        '
+        Me.no4.AutoSize = True
+        Me.no4.Location = New System.Drawing.Point(132, 125)
+        Me.no4.Name = "no4"
+        Me.no4.Size = New System.Drawing.Size(57, 50)
+        Me.no4.TabIndex = 3
+        Me.no4.Text = "4"
+        Me.no4.UseVisualStyleBackColor = True
+        '
+        'no5
+        '
+        Me.no5.AutoSize = True
+        Me.no5.Location = New System.Drawing.Point(72, 125)
+        Me.no5.Name = "no5"
+        Me.no5.Size = New System.Drawing.Size(54, 50)
+        Me.no5.TabIndex = 4
+        Me.no5.Text = "5"
+        Me.no5.UseVisualStyleBackColor = True
+        '
+        'no6
+        '
+        Me.no6.AutoSize = True
+        Me.no6.Location = New System.Drawing.Point(6, 122)
+        Me.no6.Name = "no6"
+        Me.no6.Size = New System.Drawing.Size(60, 50)
+        Me.no6.TabIndex = 5
+        Me.no6.Text = "6"
+        Me.no6.UseVisualStyleBackColor = True
+        '
+        'no7
+        '
+        Me.no7.AutoSize = True
+        Me.no7.Location = New System.Drawing.Point(132, 66)
+        Me.no7.Name = "no7"
+        Me.no7.Size = New System.Drawing.Size(57, 50)
+        Me.no7.TabIndex = 6
+        Me.no7.Text = "7"
+        Me.no7.UseVisualStyleBackColor = True
+        '
+        'no8
+        '
+        Me.no8.AutoSize = True
+        Me.no8.Location = New System.Drawing.Point(72, 66)
+        Me.no8.Name = "no8"
+        Me.no8.Size = New System.Drawing.Size(54, 50)
+        Me.no8.TabIndex = 7
+        Me.no8.Text = "8"
+        Me.no8.UseVisualStyleBackColor = True
+        '
+        'no9
+        '
+        Me.no9.AutoSize = True
+        Me.no9.Location = New System.Drawing.Point(6, 66)
+        Me.no9.Name = "no9"
+        Me.no9.Size = New System.Drawing.Size(60, 49)
+        Me.no9.TabIndex = 8
+        Me.no9.Text = "9"
+        Me.no9.UseVisualStyleBackColor = True
+        '
+        'no0
+        '
+        Me.no0.AutoSize = True
+        Me.no0.Location = New System.Drawing.Point(72, 234)
+        Me.no0.Name = "no0"
+        Me.no0.Size = New System.Drawing.Size(54, 46)
+        Me.no0.TabIndex = 9
+        Me.no0.Text = "0"
+        Me.no0.UseVisualStyleBackColor = True
+        '
+        'Button_decimal
+        '
+        Me.Button_decimal.AutoSize = True
+        Me.Button_decimal.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_decimal.Location = New System.Drawing.Point(12, 283)
+        Me.Button_decimal.Name = "Button_decimal"
+        Me.Button_decimal.Size = New System.Drawing.Size(203, 35)
+        Me.Button_decimal.TabIndex = 10
+        Me.Button_decimal.Text = "Reset"
+        Me.Button_decimal.UseVisualStyleBackColor = True
+        '
+        'Button_add
+        '
+        Me.Button_add.AutoSize = True
+        Me.Button_add.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_add.Location = New System.Drawing.Point(221, 210)
+        Me.Button_add.Name = "Button_add"
+        Me.Button_add.Size = New System.Drawing.Size(47, 39)
+        Me.Button_add.TabIndex = 11
+        Me.Button_add.Text = "+"
+        Me.Button_add.UseVisualStyleBackColor = True
+        '
+        'Button_ans
+        '
+        Me.Button_ans.AutoSize = True
+        Me.Button_ans.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_ans.Location = New System.Drawing.Point(221, 268)
+        Me.Button_ans.Name = "Button_ans"
+        Me.Button_ans.Size = New System.Drawing.Size(60, 50)
+        Me.Button_ans.TabIndex = 12
+        Me.Button_ans.Text = "="
+        Me.Button_ans.UseVisualStyleBackColor = True
+        '
+        'Button_sub
+        '
+        Me.Button_sub.AutoSize = True
+        Me.Button_sub.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_sub.Location = New System.Drawing.Point(221, 162)
+        Me.Button_sub.Name = "Button_sub"
+        Me.Button_sub.Size = New System.Drawing.Size(47, 42)
+        Me.Button_sub.TabIndex = 13
+        Me.Button_sub.Text = "-"
+        Me.Button_sub.UseVisualStyleBackColor = True
+        '
+        'Button_multi
+        '
+        Me.Button_multi.AutoSize = True
+        Me.Button_multi.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_multi.Location = New System.Drawing.Point(221, 69)
+        Me.Button_multi.Name = "Button_multi"
+        Me.Button_multi.Size = New System.Drawing.Size(47, 38)
+        Me.Button_multi.TabIndex = 14
+        Me.Button_multi.Text = "*"
+        Me.Button_multi.UseVisualStyleBackColor = True
+        '
+        'Button_divide
+        '
+        Me.Button_divide.AutoSize = True
+        Me.Button_divide.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_divide.Location = New System.Drawing.Point(221, 113)
+        Me.Button_divide.Name = "Button_divide"
+        Me.Button_divide.Size = New System.Drawing.Size(47, 37)
+        Me.Button_divide.TabIndex = 15
+        Me.Button_divide.Text = "/"
+        Me.Button_divide.UseVisualStyleBackColor = True
+        '
+        'Input
+        '
+        Me.Input.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton
+        Me.Input.Location = New System.Drawing.Point(12, 40)
+        Me.Input.Name = "Input"
+        Me.Input.Size = New System.Drawing.Size(278, 20)
+        Me.Input.TabIndex = 19
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(84, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 24)
+        Me.Label1.TabIndex = 20
+        Me.Label1.Text = "CALCULATOR"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(316, 353)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Input)
+        Me.Controls.Add(Me.Button_divide)
+        Me.Controls.Add(Me.Button_multi)
+        Me.Controls.Add(Me.Button_sub)
+        Me.Controls.Add(Me.Button_ans)
+        Me.Controls.Add(Me.Button_add)
+        Me.Controls.Add(Me.Button_decimal)
+        Me.Controls.Add(Me.no0)
+        Me.Controls.Add(Me.no9)
+        Me.Controls.Add(Me.no8)
+        Me.Controls.Add(Me.no7)
+        Me.Controls.Add(Me.no6)
+        Me.Controls.Add(Me.no5)
+        Me.Controls.Add(Me.no4)
+        Me.Controls.Add(Me.no3)
+        Me.Controls.Add(Me.no2)
+        Me.Controls.Add(Me.no1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents no1 As System.Windows.Forms.Button
+    Friend WithEvents no2 As System.Windows.Forms.Button
+    Friend WithEvents no3 As System.Windows.Forms.Button
+    Friend WithEvents no4 As System.Windows.Forms.Button
+    Friend WithEvents no5 As System.Windows.Forms.Button
+    Friend WithEvents no6 As System.Windows.Forms.Button
+    Friend WithEvents no7 As System.Windows.Forms.Button
+    Friend WithEvents no8 As System.Windows.Forms.Button
+    Friend WithEvents no9 As System.Windows.Forms.Button
+    Friend WithEvents no0 As System.Windows.Forms.Button
+    Friend WithEvents Button_decimal As System.Windows.Forms.Button
+    Friend WithEvents Button_add As System.Windows.Forms.Button
+    Friend WithEvents Button_ans As System.Windows.Forms.Button
+    Friend WithEvents Button_sub As System.Windows.Forms.Button
+    Friend WithEvents Button_multi As System.Windows.Forms.Button
+    Friend WithEvents Button_divide As System.Windows.Forms.Button
+    Friend WithEvents Input As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+
+    Dim pre_val As Double
+    Dim opr As String
+    Dim post_val As Double
+
+    Private Sub no0_Click(sender As Object, e As EventArgs) Handles no0.Click
+        Input.Text = Input.Text + "0"
+    End Sub
+
+    Private Sub Button_decimal_Click(sender As Object, e As EventArgs) Handles Button_decimal.Click
+        pre_val = Val(Input.Text)
+        Input.Text = " "
+        opr = "."
+    End Sub
+
+    Private Sub Button_ans_Click(sender As Object, e As EventArgs) Handles Button_ans.Click
+        Select Case (opr)
+            Case "+"
+                Input.Text = pre_val + Val(Input.Text)
+            Case "-"
+                Input.Text = pre_val - Val(Input.Text)
+            Case "*"
+                Input.Text = pre_val * Val(Input.Text)
+            Case "/"
+                Input.Text = pre_val / Val(Input.Text)
+           
+        End Select
+    End Sub
+
+    Private Sub no1_Click(sender As Object, e As EventArgs) Handles no1.Click
+        Input.Text = Input.Text + "1"
+    End Sub
+
+    Private Sub no2_Click(sender As Object, e As EventArgs) Handles no2.Click
+        Input.Text = Input.Text + "2"
+    End Sub
+
+    Private Sub no3_Click(sender As Object, e As EventArgs) Handles no3.Click
+        Input.Text = Input.Text + "3"
+    End Sub
+
+
+    Private Sub no4_Click(sender As Object, e As EventArgs) Handles no4.Click
+        Input.Text = Input.Text + "4"
+    End Sub
+
+    Private Sub no5_Click(sender As Object, e As EventArgs) Handles no5.Click
+        Input.Text = Input.Text + "5"
+    End Sub
+
+    Private Sub no6_Click(sender As Object, e As EventArgs) Handles no6.Click
+        Input.Text = Input.Text + "6"
+    End Sub
+
+    Private Sub Button_sub_Click(sender As Object, e As EventArgs) Handles Button_sub.Click
+        Input.Text = Input.Text + "-"
+    End Sub
+
+    Private Sub no7_Click(sender As Object, e As EventArgs) Handles no7.Click
+        Input.Text = Input.Text + "7"
+    End Sub
+
+    Private Sub no8_Click(sender As Object, e As EventArgs) Handles no8.Click
+        Input.Text = Input.Text + "8"
+    End Sub
+
+    Private Sub no9_Click(sender As Object, e As EventArgs) Handles no9.Click
+        Input.Text = Input.Text + "9"
+    End Sub
+
+    Private Sub Button_add_Click(sender As Object, e As EventArgs) Handles Button_add.Click
+        pre_val = Val(Input.Text)
+        Input.Text = " "
+        opr = "+"
+
+    End Sub
+
+    Private Sub Button_multi_Click(sender As Object, e As EventArgs) Handles Button_multi.Click
+        pre_val = Val(Input.Text)
+        Input.Text = " "
+        opr = "*"
+    End Sub
+
+    Private Sub Button_mod_Click(sender As Object, e As EventArgs)
+        pre_val = Val(Input.Text)
+        Input.Text = " "
+        opr = "%"
+    End Sub
+
+    Private Sub Button_divide_Click(sender As Object, e As EventArgs) Handles Button_divide.Click
+        pre_val = Val(Input.Text)
+        Input.Text = " "
+        opr = "/"
+    End Sub
+
+    Private Sub ButtonC_Click(sender As Object, e As EventArgs)
+        Input.Text = " "
+    End Sub
+
+    
+    
+
+End Class
